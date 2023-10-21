8316c9e1c11213e5daf849533325ddd88eec2fa0
sevincsdlii <esedliisevinc@gmail.com>
2023-10-21 04:32:16 +0400

    signlogin

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
-
+import '../axios.js'
 createApp(App).use(store).use(router).mount('#app')
